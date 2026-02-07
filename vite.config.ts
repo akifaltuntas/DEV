f536a97d964e496734bf464c6fecc7353dda2044
akifaltuntas <mehmetakifaltuntas342@gmail.com>
2026-02-07 19:12:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // GitHub Pages'te düzgün çalışması için './' veya '/repo-adi/' gereklidir.
-  // './' kullanımı çoğu durumda en güvenli yoldur.
-  base: './',
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  }
-});
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DEV/', // repo adın BÜYÜK harf
+  base: '/dev_/',
 })
+
+
